d701289ef603b7dd8fb96c85165c9dba5753281d
Royer Antonio Gomez Jimenez <rogomez@hacienda.gov.do>
2024-09-27 15:44:39 -0400

    save-progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
+  theme: {
     //   colors: {
     //     'blue': '#1fb6ff',
     //     'purple': '#7e5bef',
@@ -12,18 +12,21 @@ module.exports = {
     //     'gray': '#8492a6',
     //     'gray-light': '#d3dce6',
     //   },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      extend: {
-        spacing: {
-          '8xl': '96rem',
-          '9xl': '128rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
-  }
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      divideColor: {
+        DEFAULT: "#0a0a0a",
+      },
+    },
+  },
+};
