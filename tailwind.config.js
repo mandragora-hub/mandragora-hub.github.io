4779572fcba24166f8feecea64a1289e5ab5a9bd
Royer Antonio Gomez Jimenez <rogomez@hacienda.gov.do>
2024-10-02 15:46:40 -0400

    save-progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  theme: {
-    //   colors: {
-    //     'blue': '#1fb6ff',
-    //     'purple': '#7e5bef',
-    //     'pink': '#ff49db',
-    //     'orange': '#ff7849',
-    //     'green': '#13ce66',
-    //     'yellow': '#ffc82c',
-    //     'gray-dark': '#273444',
-    //     'gray': '#8492a6',
-    //     'gray-light': '#d3dce6',
-    //   },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
+tailwind = {
+  config: {
+    theme: {
+      //   colors: {
+      //     'blue': '#1fb6ff',
+      //     'purple': '#7e5bef',
+      //     'pink': '#ff49db',
+      //     'orange': '#ff7849',
+      //     'green': '#13ce66',
+      //     'yellow': '#ffc82c',
+      //     'gray-dark': '#273444',
+      //     'gray': '#8492a6',
+      //     'gray-light': '#d3dce6',
+      //   },
+      fontFamily: {
+        sans: ["Lato", "system-ui", "sans-serif"],
+        // serif: ["EB Garamond", "serif"],
       },
-      borderRadius: {
-        "4xl": "2rem",
+      extend: {
+        spacing: {
+          "8xl": "96rem",
+          "9xl": "128rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
       },
-      divideColor: {
-        DEFAULT: "#0a0a0a",
-      },
-    },
-  },
-};
+    }
+  }
+}
