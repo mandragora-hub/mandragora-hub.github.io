5caa4deef1c0a54c376650b5991e7d9710e32b92
Royer Gomez <royer01gomez@gmail.com>
2024-11-15 15:56:40 -0400

    Simplified design

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+tailwind = {
+  config: {
+    theme: {
+      //   colors: {
+      //     'blue': '#1fb6ff',
+      //     'purple': '#7e5bef',
+      //     'pink': '#ff49db',
+      //     'orange': '#ff7849',
+      //     'green': '#13ce66',
+      //     'yellow': '#ffc82c',
+      //     'gray-dark': '#273444',
+      //     'gray': '#8492a6',
+      //     'gray-light': '#d3dce6',
+      //   },
+      container: {
+        padding: "10rem",
+      },
+      fontFamily: {
+        sans: ["Lato", "system-ui", "sans-serif"],
+        // serif: ["EB Garamond", "serif"],
+      },
+      extend: {
+        spacing: {
+          "8xl": "96rem",
+          "9xl": "128rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+        screens: {
+          print: { raw: "print" },
+        },
+      },
+    },
+  },
+};
